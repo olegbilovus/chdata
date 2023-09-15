2101f9f830830348f5bf9f1efa74425964467a01
Oleg Bilovus <68842946+OB-UNISA@users.noreply.github.com>
2023-09-15 20:04:59 +0200

    fix assets for images

--- lib/view/search/constants.dart
+++ lib/view/search/constants.dart
@@ -1,8 +1,7 @@
 import 'package:chdata/models/item/constants.dart';
 import 'package:chdata/models/item/enum/equipment_slot.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 const widthSB = 8.0;
 const textStyleField = TextStyle(
@@ -20,4 +19,4 @@ final emptyValues = [
   EquipmentSlot.unequipable.name.capitalize
 ];
 
-const imagesAssetsDir = kIsWeb ? 'images/zones' : 'assets/images/zones';
+const imagesAssetsDir = 'assets/images/zones';
