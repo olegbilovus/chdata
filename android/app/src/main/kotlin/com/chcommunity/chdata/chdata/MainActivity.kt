b31d7b6fb6245f5bf70caafabed81de0f57b61a5
Oleg Bilovus <68842946+olegbilovus@users.noreply.github.com>
2024-07-19 19:00:45 +0200

    dependencies update

--- android/app/src/main/kotlin/com/chcommunity/chdata/chdata/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.chcommunity.chdata.chdata
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/chdata/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chdata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
