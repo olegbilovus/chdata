38adb8f12f8d8af0132d7c90bc5773d6b00a9265
Oleg Bilovus <68842946+olegbilovus@users.noreply.github.com>
2025-11-30 02:23:01 +0100

    Update loc.dart

--- lib/extensions/buildcontext/loc.dart
+++ lib/extensions/buildcontext/loc.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart' show BuildContext;
-import 'package:chdata/l10n/app_localizations.dart
+import 'package:chdata/l10n/app_localizations.dart'
     show AppLocalizations;
 
 extension Localization on BuildContext {
